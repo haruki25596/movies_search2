bccbd5a1ff26f6e3614820d447c650a4e04e1f85
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-06-08 11:00:29 +0000

    viewの作成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
-  
+
   require 'themoviedb-api'
   Tmdb::Api.key("6ce86077881a441d243745e068c0f6dc")
-  Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
+  Tmdb::Api.language("ja") # 映画情報の表示の際の言語設定を日本語にできる
 
 
   def search
+    @tmdbs = Tmdb::Search.movie(params[:q])
   end
 
   def show

8f0ded366738828b59f942f796322215c14bba2e
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-29 13:25:12 +0000

    review_comment_controllerにcreateアクション追加

--- app/controllers/review_comment_controller.rb
+++ app/controllers/review_comment_controller.rb
@@ -4,4 +4,14 @@ class ReviewCommentController < ApplicationController
     @review_comments = ReviewComment.where(review_id: params[:review_id]).page(params[:page]).per(8)
     @review = Review.find(params[:review_id])
   end
+  
+  def create
+    review = Review.find(params[:review_id])
+    comment = current_user.review_comments.new(review_comment_params)
+    comment.review_id = review.id
+    @review = comment.review
+    comment.save
+    @review.create_notification_review_comment!(current_user, review.user_id, review.id)
+    redirect_to new_review_review_comment_path(review)
+  end
 end

648e33f9a290e7e9c4fe6992b6b0df6b87714f97
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-29 13:24:18 +0000

    review_comment_controllerにnewアクション追加

--- app/controllers/review_comment_controller.rb
+++ app/controllers/review_comment_controller.rb
@@ -1,4 +1,7 @@
 class ReviewCommentController < ApplicationController
   def new
+    @review_comment = ReviewComment.new
+    @review_comments = ReviewComment.where(review_id: params[:review_id]).page(params[:page]).per(8)
+    @review = Review.find(params[:review_id])
   end
 end

178561cb095571d509aa0b426558fb1bbd262679
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-30 19:55:45 +0000

    ReviewFavoritesコントローラーにcreateアクション追加

--- app/controllers/review_favorites_controller.rb
+++ app/controllers/review_favorites_controller.rb
@@ -1,4 +1,11 @@
 class ReviewFavoritesController < ApplicationController
   
+  def create
+    review = Review.find(params[:review_id])
+    favorite = current_user.review_favorites.new(review_id: review.id)
+    favorite.save
+    review.create_notification_review_favorite!(current_user.id, review.user_id, review.id)
+    redirect_to movie_path(review.movie_id)
+  end
   
 end

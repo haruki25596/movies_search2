51e5dc95ec0e4e3f35070f53496de7178f8cd30a
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-30 19:56:37 +0000

    ReviewFavoritesコントローラーにdestroyアクション追加

--- app/controllers/review_favorites_controller.rb
+++ app/controllers/review_favorites_controller.rb
@@ -8,4 +8,11 @@ class ReviewFavoritesController < ApplicationController
     redirect_to movie_path(review.movie_id)
   end
   
+  def destroy
+    review = Review.find(params[:review_id])
+    favorite = current_user.review_favorites.find_by(review_id: review.id)
+    favorite.destroy
+    redirect_to movie_path(review.movie_id)
+  end
+  
 end

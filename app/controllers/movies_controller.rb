b549d5778782237e7fd73c4134151ace117be0f8
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-08-01 05:34:38 +0000

    envファイルにAPIキーを記載

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :redirect_root, except: [:index, :show, :search]
 
   require 'themoviedb-api'
-  Tmdb::Api.key("6ce86077881a441d243745e068c0f6dc")
+  Tmdb::Api.key("TMDB_KEY")
   Tmdb::Api.language("ja") # 映画情報の表示の際の言語設定を日本語にできる
 
 
